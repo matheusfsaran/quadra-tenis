6b16c2adf45a18ac01968ddccfab07bc9482e4bd
matheusfsaran <matheusfsaran@gmail.com>
2026-05-13 09:03:18 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -3,4 +3,12 @@ import { createClient } from '@supabase/supabase-js'
 const SUPABASE_URL = 'https://tbzjxcdaxmvgjjlukncw.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRiemp4Y2RheG12Z2pqbHVrbmN3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg1ODE3MjksImV4cCI6MjA5NDE1NzcyOX0.27Jj18oqizAa3Hq24dh5Qxh5imy1ceUJQg-oSLzeA-I'
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
+  db: { schema: 'public' },
+  global: {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  }
+})
